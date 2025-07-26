683a643772a2a39c0fdff3972fb6ccf33edbb1d9
Your Name <youremail@example.com>
2025-07-27 00:56:17 +0600

    create user model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,44 @@
+import { model, Schema } from "mongoose";
+import { IAuthProvider, isActive, IUser, Role } from "./user.interface";
+import { escapeRegex } from "zod/v4/core/util.cjs";
+
+
+
+const authProviderSchema = new Schema<IAuthProvider>({
+    provider: {type: String, required:true},
+    providerId: {type: String, required:true},
+},{
+    versionKey: false,
+    _id: false
+})
+
+const userSchema = new Schema<IUser>({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique:true},
+    password: {type:String},
+    role: {
+        type: String,
+        enum: Object.values(Role),
+        default: Role.USER
+    },
+    phone: {type:String},
+    picture:{type:String},
+    address: {type:String},
+    isDeleted: {type:String},
+    isActive: {
+        type:String,
+        enum:Object.values(isActive),
+        default:isActive.ACTIVE,
+    },
+    isVerified: {type:Boolean, default: false},
+    auths:[authProviderSchema],
+    bookings:{
+
+    }
+
+},{
+    timestamps: true,
+    versionKey: false
+})
+
+export const User = model<IUser>("User", userSchema)
