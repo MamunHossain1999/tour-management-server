7ccd0d60717663b99212565ededa2d6b68246677
Your Name <youremail@example.com>
2025-08-01 10:24:43 +0600

    refactor user controller to use catchAsync for error handling and improve response structure

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,25 +1,34 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// src/controllers/user.controller.ts
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import { catchAsync } from "../../../ulits/catchAsynce";
 import { UserService } from "./user.service";
+import { success } from "zod";
 
-const creatreUser = async (req: Request, res: Response) => {
-  try {
-   const user = await UserService.createUserService(req.body) //user service theke ana hoyese
-  
-    res.status(httpStatus.CREATED).json({
-        message: "User Created Successfully",
-        user
-    });
-  } catch (err: any) {
-    res.status(httpStatus.BAD_REQUEST).json({
-      message: `Something Went Wrong ${err.message}`,
-      err,
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const user = await UserService.createUserService(req.body);
+
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    message: "User Created Successfully",
+    data: user,
+  });
+});
+
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserService.getAllUsers();
+
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "All Users Retrieved Successfully",
+      data: users,
     });
   }
+);
+
+export const UserController = {
+  createUser,
+  getAllUsers,
 };
-
-
-export const UserControllers = {
-    creatreUser
-}
