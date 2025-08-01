f64a16374dc39a80eb7d7a30c2b5cbf279edde92
Your Name <youremail@example.com>
2025-08-01 09:09:44 +0600

    refactor user creation logic by moving to a dedicated service

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 import httpStatus from "http-status-codes";
-import { User } from "./user.model";
+import { UserService } from "./user.service";
 
 const creatreUser = async (req: Request, res: Response) => {
   try {
-    const { name, email } = req.body;
-    const user = await User.create({
-      name,
-      email,
-    });
+   const user = await UserService.createUserService(req.body) //user service theke ana hoyese
+  
     res.status(httpStatus.CREATED).json({
         message: "User Created Successfully",
         user
